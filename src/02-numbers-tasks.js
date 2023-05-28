d280a24fc8dfd33a78ee8180ac8527cd77da011d
sorvigolovaa <sbknugaara@gmail.com>
2023-05-28 20:31:26 +0500

    refactor: fix code

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -228,7 +228,7 @@ function isPrime(n) {
   }
 
   const sqrt = Math.sqrt(n);
-  for (let i = 2; i <= sqrt; i + 1) {
+  for (let i = 2; i <= sqrt; i += 1) {
     if (n % i === 0) {
       return false;
     }
